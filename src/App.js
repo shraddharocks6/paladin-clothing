c03acfacc363f265ac2533988c7d3170293b39ac
Shraddha Pandey <shraddha6pandey@gmail.com>
2021-01-06 09:05:31 +0530

    Update our munu items to navigate withRouter Higher order component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from "./pages/homepage/homepage.component";
 
-import './App.css';
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path ="/shop/hats" component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
