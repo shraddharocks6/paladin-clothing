9c415da8f28610aece9bd49c346ac90eba9b7b98
Shraddha Pandey <shraddha6pandey@gmail.com>
2021-01-09 14:13:17 +0530

    Adding header cpmponent and links to the home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component.jsx'
 
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      {/* <HomePage /> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path ="/shop/hats" component={HatsPage}/>
+        <Route path ="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./collection-item.styles.scss";
+
+// const CollectionItem = ({ id, name, price, imageUrl }) => (
+//   <div className="collection-item">
+//     <div
+//       className="image"
+//       style= {{
+//         backgroundImage: `url(${imageUrl})`,
+//       }}
+//     />
+//     <div className = "collection-footer">
+//       <span className="name">{name}</span>
+//       <span className="price">{price}</span>
+//     </div>
+//   </div>
+// );
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{backgroundImage: `url(${imageUrl})`}}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+  //<div>{console.log(imageUrl)}</div>
+);
+
+export default CollectionItem;
