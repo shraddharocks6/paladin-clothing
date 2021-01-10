13916439d21ec4ad26a53ddd756d0163dbade5f8
Shraddha Pandey <shraddha6pandey@gmail.com>
2021-01-11 04:22:22 +0530

    Integration of firebase complete, signIn page complete with signIn with google working

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import {auth} from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -19,11 +20,14 @@ const Header = () => (
         CONTACT
       </Link>
       <Link className="option" to="/signin">
-        SIGN IN 
+        {
+          currentUser ?
+          <div className="option" onClick={() => auth.signOut()}>SIGN OUT </div>
+          :
+          <Link className='option'></Link>
+        }
       </Link>
-      <Link className="option" to="/bag">
-        BAG 
-      </Link>
+    
     </div>
   </div>
 );
