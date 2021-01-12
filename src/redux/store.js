85d18bf87a7eb513f1e52129a4b0d51beaf2b0e8
Shraddha Pandey <shraddha6pandey@gmail.com>
2021-01-12 23:44:00 +0530

    Redux added, userReducer and userActions ipmlemented to header and app components

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,4 +5,6 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddlewares(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
